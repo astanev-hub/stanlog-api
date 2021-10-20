33863cd89db29f44be5334d41b06d068547930c9
Alexandre Stanev <astanev@hotmail.com>
2021-10-20 17:17:27 -0300

    Persistence (JPA, Spring Data)

--- src/main/java/com/stanev/stanlog/api/controller/ClienteController.java
+++ src/main/java/com/stanev/stanlog/api/controller/ClienteController.java
@@ -1,13 +1,23 @@
 package com.stanev.stanlog.api.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stanev.stanlog.domain.model.Cliente;
+import com.stanev.stanlog.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
-	public String listar() {
-		return "Teste";
+	public List<Cliente> listar() {
+		return clienteRepository.findAll();
 	}
 }
